a9afce39ad63659b2d4c7c3d5e3b35bf6e2c0d04
Fabian Jakobs <fabian@c9.io>
2014-09-26 13:58:09 +0000

    update client unit tests

--- gotoline_test.js
+++ gotoline_test.js
@@ -17,7 +17,7 @@ require(["lib/architect/architect", "lib/chai/chai"], function (architect, chai)
         },
         
         "plugins/c9.core/ext",
-        "plugins/c9.core/http",
+        "plugins/c9.core/http-xhr",
         "plugins/c9.core/util",
         {
             packagePath: "plugins/c9.core/settings",
